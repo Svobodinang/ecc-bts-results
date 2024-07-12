687281a90e38b4909321b9d2ff643e782f6b22ce
Надежда Свободина <nadezdasvobodina@MacBook-Pro-Nadezda.local>
2024-07-12 14:13:11 +0300

    init project

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import './app.scss';
+// import addInviewByScroll from './js/addInviewByScroll';
+
+// import data from './data/basic.json';
+
+function start() {
+    // const inview = new Inview();
+    // const swipers = new Swipers();
+
+    // window.addEventListener('load', () => {
+    //     addInviewByScroll();
+    // });
+}
+
+start();
